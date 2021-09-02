cfaac34f89ecf28e17395431b433c963b31424b2
MeggieStenback <ms42524@srcs.org>
2021-09-02 15:13:40 -0700

    Finished Game
    Finished Crab Project

--- Crab.java
+++ Crab.java
@@ -1,14 +1,77 @@
 import greenfoot.*;
 
-/**
- * This class defines a crab. Crabs live on the beach.
- */
+    /**
+    * The class controls the Crab object
+    * @author: Meggie Stenback
+    * @version: 8/26/2021
+    */
 public class Crab extends Actor
 {
+     private int numOfWorms = 8;
+    // This method repeats the following actions
     public void act()
     {
-        // Add your action code here
+          
+         move(3);
+         turnAtEdge();
+         checkKeyPress();
+         onCollision();
+     
+    }
+
+    // Moves the Crab
+
+    // Turns the Crab at the edge
+    private void turnAtEdge( )
+    {
+        if(isAtEdge())
+        {
+            turn(-50);
+        }
+    }
+    // Checks for user key presses so user can turn the Crab
+    private void checkKeyPress( )
+    {
+        if(Greenfoot.isKeyDown("right"))
+        {
+            turn(4);
+        }
+    
+        if(Greenfoot.isKeyDown("left"))
+        {
+            turn(-4);
+        }
+        
+                if(Greenfoot.isKeyDown("up"))
+        {
+            Greenfoot.delay(60);
+        }
+    }
+    // Checks for collisions with other objects
+    private void onCollision ( )
+    {   
+        if(isTouching(Worm.class))
+        {
+            removeTouching(Worm.class);
+            Greenfoot.playSound("slurp.wav");
+            numOfWorms --;
+            // Winning the game
+            if(numOfWorms ==0)
+
+            {
+             Greenfoot.setWorld(new WinSplash());
+                Greenfoot.playSound("fanfare.wav");
+                Greenfoot.stop();   
+            }
+            if(getWorld( ).getObjects(Worm.class).size()==0)
+            {
+                
+            }
+        }
+        if(isTouching(Lobster.class))
+        {
+            Greenfoot.playSound("au.wav");
+            Greenfoot.stop( );
+        }
     }
 }
-
-
